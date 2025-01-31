42ac385221ddf59dd15c4f5fe0fe9afb3eb8c025
joshweinstock <jyweinstock@uchicago.edu>
2025-01-31 03:05:59 +0000

    calc_1

--- app.rb
+++ app.rb
@@ -2,7 +2,45 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  @number_square=params.fetch("number").ti_i
-  @answer=[]
-erb(:homepage)
+erb(:square_new)
 end
+
+
+get("/square/new") do
+erb(:square_new)
+end
+
+get("/square/results") do
+@number = params.fetch("number").to_f
+@square = @number ** 2
+erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root_new)
+  end
+  
+get("/square_root/results") do
+  @number = params.fetch("number").to_f
+  @square_root = @number ** 0.5
+  erb(:square_root_results)
+  end
+
+  get("/payment/new") do
+    erb(:payment_new)
+    end
+    
+  get("/payment/results") do
+    @years = params.fetch("years_input").to_f
+    @apr = params.fetch("apr_input").to_f
+    @principal = params.fetch("principal_input").to_f
+    @apr_rate = (@apr/100)/12
+    @monthly = @years *12
+    @payment = ( @apr_rate * @principal )/(1-(1+ @apr_rate )**- @monthly )
+    
+    @user_apr = @apr_rate.to_fs(:percentage, {:precision=>4})
+    @user_payment = @payment.to_fs(:currency)
+    @user_principal = @principal.to_fs(:currency)
+
+    erb(:payment_results)
+    end
